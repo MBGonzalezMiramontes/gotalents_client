7e1294495c5be84deddb7e2a593c87332256bde4
MBGonzalezMiramontes <begonzalezmiramontes@gmail.com>
2024-05-21 16:12:39 -0300

    http x https

--- src/utils/NetworkingUtils.js
+++ src/utils/NetworkingUtils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const GoTaletsServer = axios.create({
-    baseURL:"http://93.127.210.195:3001",
+    baseURL:"https://93.127.210.195:3001",
 });
 
 export default GoTaletsServer;
