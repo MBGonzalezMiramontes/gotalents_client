44c46f6c4f871fbd95028321818c38dd9edd6eb6
MBGonzalezMiramontes <begonzalezmiramontes@gmail.com>
2024-05-21 17:34:37 -0300

    C:/Program Files/Git/nodeapp

--- src/utils/NetworkingUtils.js
+++ src/utils/NetworkingUtils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const GoTaletsServer = axios.create({
-    baseURL:"https://93.127.210.195:3001",
+    baseURL:"https://93.127.210.195/nodeapp",
 });
 
 export default GoTaletsServer;
